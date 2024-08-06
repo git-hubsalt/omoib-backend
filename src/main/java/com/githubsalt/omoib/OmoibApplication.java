dd18517718f6c29579800f10daa2f3abaa69687c
sinam7 <muwon777@naver.com>
2024-08-06 16:28:42 +0900

    chore: project initial setup

---
+++ src/main/java/com/githubsalt/omoib/OmoibApplication.java
@@ -0,0 +1,13 @@
+package com.githubsalt.omoib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmoibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmoibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubsalt/omoib/OmoibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubsalt.omoib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmoibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
