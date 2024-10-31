951c04cefbc01e01a1605ac4825a438be9986815
sinam7 <muwon777@naver.com>
2024-10-31 23:56:22 +0900

    develop(RecommendationResponseDTO): dto 필드 타입 변경
    옷 id 대신 옷 dto를 보내주는 것으로 변경

--- src/main/java/com/githubsalt/omoib/codyrecommendation/dto/RecommendationResponseDTO.java
+++ src/main/java/com/githubsalt/omoib/codyrecommendation/dto/RecommendationResponseDTO.java
@@ -1,8 +1,9 @@
 package com.githubsalt.omoib.codyrecommendation.dto;
 
+import com.githubsalt.omoib.clothes.dto.GetClothesResponseDTO;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-public record RecommendationResponseDTO(@NotNull Long userId, List<Long> clothesIdList) {
+public record RecommendationResponseDTO(@NotNull Long userId, List<GetClothesResponseDTO.ClothesItemDTO> clothesList) {
 }
