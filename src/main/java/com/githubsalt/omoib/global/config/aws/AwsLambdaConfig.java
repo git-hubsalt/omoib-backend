37f0139a3f7030ad16762513ceb6b1b7190c6778
TaegeunYou <dbxorms2310@naver.com>
2024-11-06 21:00:31 +0900

    refactor(AwsLambdaConfig): amazonSQS 추가

--- src/main/java/com/githubsalt/omoib/global/config/aws/AwsLambdaConfig.java
+++ src/main/java/com/githubsalt/omoib/global/config/aws/AwsLambdaConfig.java
@@ -1,7 +1,10 @@
 package com.githubsalt.omoib.global.config.aws;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.lambda.AWSLambda;
 import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +16,13 @@ public class AwsLambdaConfig {
         return AWSLambdaClientBuilder.standard()
             .withRegion("ap-northeast-2")
             .build();
+    }
+
+    @Bean
+    public AmazonSQS amazonSQS() {
+        return AmazonSQSClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                        "https://sqs.ap-northeast-2.amazonaws.com", "ap-northeast-2"))
+                .build();
     }
 }
