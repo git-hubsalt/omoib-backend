363cf1cf8ac9c89d8a0ddd53695c3717115bb420
TaegeunYou <dbxorms2310@naver.com>
2024-11-06 21:12:06 +0900

    refactor(AwsLambdaConfig): credentials 추가

--- src/main/java/com/githubsalt/omoib/global/config/aws/AwsLambdaConfig.java
+++ src/main/java/com/githubsalt/omoib/global/config/aws/AwsLambdaConfig.java
@@ -1,28 +1,27 @@
 package com.githubsalt.omoib.global.config.aws;
 
-import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.lambda.AWSLambda;
 import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AwsLambdaConfig {
 
-    @Bean
-    public AWSLambda awsLambda() {
-        return AWSLambdaClientBuilder.standard()
-            .withRegion("ap-northeast-2")
-            .build();
-    }
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
 
     @Bean
-    public AmazonSQS amazonSQS() {
-        return AmazonSQSClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
-                        "https://sqs.ap-northeast-2.amazonaws.com", "ap-northeast-2"))
+    public AWSLambda awsLambda() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        return AWSLambdaClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withRegion("ap-northeast-2")
                 .build();
     }
 }
