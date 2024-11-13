70258135bc1e054a5fbb1e587f7377541d5ca589
sinam7 <wjdtjddnjs7@gmail.com>
2024-11-13 20:45:15 +0900

    temp(aws): aws 피팅 endpoint 호출 관련 오류 수정을 위한 임시 로깅 코드

--- src/main/java/com/githubsalt/omoib/global/config/RestTemplateConfig.java
+++ src/main/java/com/githubsalt/omoib/global/config/RestTemplateConfig.java
@@ -1,21 +1,40 @@
 package com.githubsalt.omoib.global.config;
 
+import com.githubsalt.omoib.global.LoggingRequestInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class RestTemplateConfig {
+
+    private final LoggingInterceptor loggingInterceptor;
+
     @Bean
     public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(createRequestFactory()));
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<>();
+        interceptors.add(new LoggingRequestInterceptor());
+        restTemplate.setInterceptors(interceptors);
+//        // LoggingInterceptor 추가
+        restTemplate.setInterceptors(List.of(loggingInterceptor));
+
+        return restTemplate;
+    }
+
+    private ClientHttpRequestFactory createRequestFactory() {
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
         requestFactory.setConnectTimeout(5000);
         requestFactory.setReadTimeout(5000);
-
-        restTemplate.setRequestFactory(requestFactory);
-
-        return restTemplate;
+        return requestFactory;
     }
 }
