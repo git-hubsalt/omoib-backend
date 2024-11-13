f19c44a671a297c2eaa1a5fec7282edf806bea92
sinam7 <wjdtjddnjs7@gmail.com>
2024-11-13 16:51:48 +0900

    develop(virtual-fitting): 가상 피팅 API dto 수정
    사용자 정보 업데이트 후 db 반영 누락 수정
    AI 요청 dto로 상의 1개, 하의 1개만 입력받도록 수정

--- src/main/java/com/githubsalt/omoib/virtualfitting/VirtualFittingService.java
+++ src/main/java/com/githubsalt/omoib/virtualfitting/VirtualFittingService.java
@@ -1,15 +1,19 @@
 package com.githubsalt.omoib.virtualfitting;
 
-import com.githubsalt.omoib.aws.dto.SqsFittingResponseMessageDTO;
+import com.githubsalt.omoib.aws.s3.PresignedURLBuilder;
 import com.githubsalt.omoib.clothes.domain.Clothes;
 import com.githubsalt.omoib.clothes.dto.BriefClothesDTO;
+import com.githubsalt.omoib.clothes.enums.ClothesType;
 import com.githubsalt.omoib.clothes.service.ClothesService;
 import com.githubsalt.omoib.history.History;
 import com.githubsalt.omoib.history.HistoryService;
 import com.githubsalt.omoib.history.HistoryType;
 import com.githubsalt.omoib.history.enums.HistoryStatus;
+import com.githubsalt.omoib.user.domain.User;
+import com.githubsalt.omoib.user.service.UserService;
 import com.githubsalt.omoib.virtualfitting.dto.FittingAIRequestDTO;
 import com.githubsalt.omoib.virtualfitting.dto.FittingRequestDTO;
+import com.githubsalt.omoib.virtualfitting.dto.SqsFittingResponseMessageDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -24,6 +28,8 @@ public class VirtualFittingService {
 
     private final ClothesService clothesService;
     private final HistoryService historyService;
+    private final UserService userService;
+    private final PresignedURLBuilder presignedURLBuilder;
 
     public void fitting(Long userId, FittingRequestDTO requestDTO) {
 
@@ -32,29 +38,43 @@ public class VirtualFittingService {
         }
 
         List<BriefClothesDTO> briefClothes = requestDTO.clothes();
-        List<Clothes> clothesList = new ArrayList<>();
+        Clothes upper = null, lower = null;
         for (BriefClothesDTO briefClothesDTO : briefClothes) {
             Clothes clothesItem = clothesService.getClothes(briefClothesDTO.id());
-            clothesList.add(clothesItem);
-            if (clothesItem == null) {
-                throw new IllegalArgumentException("존재하지 않는 옷입니다.");
+            if (clothesItem.getClothesType() == ClothesType.upper && upper == null) {
+                upper = clothesItem;
+            } else if (clothesItem.getClothesType() == ClothesType.lower && lower == null) {
+                lower = clothesItem;
+            }
+
+            if (upper != null && lower != null) {
+                break;
             }
         }
 
-        String timestamp = historyService.createPendingFittingHistory(userId, clothesList);
+        if (upper == null || lower == null) {
+            throw new IllegalArgumentException("상의와 하의를 모두 선택해주세요.");
+        }
+        String timestamp = historyService.createPendingFittingHistory(userId, new ArrayList<>(List.of(upper, lower)));
 
+        User user = userService.findUser(userId).orElseThrow();
         FittingAIRequestDTO aiRequestDTO = new FittingAIRequestDTO(
-                userId,
+                userId.toString(),
                 timestamp,
-                requestDTO.clothes()
-        );
+                presignedURLBuilder.buildGetPresignedURL(user.getRowImagePath()).toString(),
+                presignedURLBuilder.buildGetPresignedURL(upper.getImagePath()).toString(),
+                presignedURLBuilder.buildGetPresignedURL(lower.getImagePath()).toString(),
+                presignedURLBuilder.buildGetPresignedURL(String.format("users/%s/masking/%s/overall.jpg",
+                        userId, user.getLastMaskingTimestamp())).toString(),
+                "overall");
+
 
         // TODO 피팅 모델 endpoint 호출
     }
 
     public void response(SqsFittingResponseMessageDTO message) {
         History pendingHistory = historyService.findPendingHistory(message.userId(), HistoryType.FITTING);
-        pendingHistory.setFittingImageURL(message.imageURL());
+        pendingHistory.setFittingImageURL("/users/" + message.userId() + "/vton_result/" + message.timestamp() + "result.jpg");
         pendingHistory.setStatus(HistoryStatus.COMPLETED);
         historyService.updateHistory(pendingHistory);
     }
