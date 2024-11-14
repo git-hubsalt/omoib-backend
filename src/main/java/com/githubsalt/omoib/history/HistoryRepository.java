32d61c2058172b13f6fa891aafb7ee5bbd535c9e
sinam7 <wjdtjddnjs7@gmail.com>
2024-11-15 02:05:31 +0900

    temp: 가상피팅 시 pending 있어도 다시 요청 가능하게 수정

--- src/main/java/com/githubsalt/omoib/history/HistoryRepository.java
+++ src/main/java/com/githubsalt/omoib/history/HistoryRepository.java
@@ -15,5 +15,6 @@ public interface HistoryRepository extends JpaRepository<History, Long> {
 
     boolean existsByUserIdAndStatus(Long userId, HistoryStatus historyStatus);
     Optional<History> findByUserIdAndStatusAndType(Long userId, HistoryStatus historyStatus, HistoryType historyType);
+    List<History> findAllByUserIdAndStatusAndType(Long userId, HistoryStatus historyStatus, HistoryType historyType);
     List<History> findAllByUserIdAndNotifyStatus(Long userId, NotifyStatus notifyStatus);
 }
